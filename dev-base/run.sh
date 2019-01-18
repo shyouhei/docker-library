78bea96a5676f5a7a41a4ceddf1b66c0e512da99
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2019-01-18 10:36:48 +0900

    split Dockerfile into two

--- dev-base/run.sh
+++ dev-base/run.sh
@@ -2,11 +2,17 @@
 # This is almost a straight translation of docker-compose.yml 
 exec sudo docker run \
      --rm \
+     -ip6 \
      -it \
      --init \
      -e DISPLAY \
+     -e TZ \
      --mount \
        type=bind,source=/tmp/.X11-unix,target=/tmp/.X11-unix \
      --mount \
+       type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
+     --mount \
+       type=bind,source=/etc/timezone,target=/etc/timezone,readonly \
+     --mount \
        type=bind,source=/home/shyouhei,target=/home/shyouhei \
-     shyouhei/dev-base
+     shyouhei/dev-tmux
