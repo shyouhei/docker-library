03af9151b329d610b460023c9c0e6b118a95febe
卜部昌平 <shyouhei@ruby-lang.org>
2020-03-04 12:20:31 +0900

    dev-base now based on shyouhei/c-compilers

--- dev-base/run.sh
+++ dev-base/run.sh
@@ -1,24 +1,21 @@
 #! /bin/sh
-# This is almost a straight translation of docker-compose.yml 
+# sudo docker volume create dev-base
 exec sudo docker run \
      --rm \
      -ip6 \
      -it \
      --init \
-     -e DISPLAY \
      -e TZ \
-     --mount \
-       type=bind,source=/tmp/.X11-unix,target=/tmp/.X11-unix \
      --mount \
        type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
      --mount \
        type=bind,source=/etc/timezone,target=/etc/timezone,readonly \
      --mount \
-       type=bind,source=/home/shyouhei,target=/home/shyouhei \
+       source=dev-base,target=/home/shyouhei \
      --security-opt \
        seccomp=unconfined \
      --cap-add=SYS_PTRACE \
      --cap-add=SYS_ADMIN \
      --dns=8.8.8.8 \
      --dns=8.8.4.4 \
-     shyouhei/dev-tmux
+     shyouhei/dev-base
